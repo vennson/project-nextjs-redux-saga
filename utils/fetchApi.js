40437f8a5acdcefd826b399acc5b66149cdd6fb1
Benson Miakoun <bmiakoun@gmail.com>
2021-12-09 19:15:24 +0800

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      'x-rapidapi-key': 'ad776fefb1msh48853a5ce849503p192853jsn62e392df1729'
+    }
+  })
+
+  return data
+}
+
